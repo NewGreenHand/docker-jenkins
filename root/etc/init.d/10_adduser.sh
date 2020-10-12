c18fd7c17b71e44245aaa3094c5a0796601a2328
坑飞 <myisafei@gmail.com>
2020-10-12 22:41:34 +0800

    Update 10_adduser.sh

--- root/etc/init.d/10_adduser.sh
+++ root/etc/init.d/10_adduser.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-GROUPID=${GROUPID:-65537}
+# GROUPID=${GROUPID:-65537}
 
-echo "docker:x:$GROUPID:jenkins" >> /etc/group
+# echo "docker:x:$GROUPID:jenkins" >> /etc/group
+
+PUID=${PUID:-1029}
+PGID=${PGID:-65537}
+
+groupmod -o -g "$PGID" jenkins
+usermod -o -u "$PUID" jenkins 
